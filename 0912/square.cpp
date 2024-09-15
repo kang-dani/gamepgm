c9de0f5702c7da0c8156fb29b90ac660a35138f6
kang-dani <daeun4001@gmail.com>
2024-09-15 19:49:54 +0900

    square.cpp edit

--- 0912/square.cpp
+++ 0912/square.cpp
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <locale>
 
 void draw_square(int size);
 
 int main(void) {
+	setlocale(LC_ALL, "");
 	int n;
 	
 	printf("Draw SQUARE\n\n");
@@ -15,34 +17,38 @@ int main(void) {
 }
 void draw_square(int size) {
 	int i, j;
-	unsigned char a=0xa6;
+	unsigned char a = 0xa6;
 	unsigned char b[7];
 	
-	for(i=1; i<7; i++)
-		b[i]=0xa0+i;
+	for(i = 0; i < 7; i++)
+		b[i] = 0xa0 + i;
 		
-	printf("%c%c",a, b[3]);
+	// 정사각형의 상단
+	printf("%c%c", a, b[3]);
 	
-	for(i=0; i<size; i++)
+	for(i = 0; i < size; i++)
 		printf("%c%c", a, b[1]);
 		
 	printf("%c%c", a, b[4]);
 	printf("\n");
 	
-	for(i=0; i<size; i++) 
+	
+	//정사각형의 중간 부분
+	for(i = 0; i < size; i++) 
 	{
 		printf("%c%c", a, b[2]);
 		
-		for(j=0; j<size; j++)
+		for(j = 0; j < size; j++)
 			printf(" ");
 			
-		printf("%c%c",a, b[2]);
+		printf("%c%c", a, b[2]);
 		printf("\n");
 	}
 	
+	//정사각형의 하단
 	printf("%c%c", a, b[6]);
 	
-	for(i=0; i<size; i++)
+	for(i = 0; i < size; i++)
 		printf("%c%c", a, b[1]);
 		
 	printf("%c%c", a, b[5]);
